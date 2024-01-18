7872e21669a73452b485628f55fce4ab2a8f9b75
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 02:59:39 +0900

    Create fmkoreaPlus.js

---
+++ fmkoreaPlus.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         fmkoreaPlus
+// @name:ko      펨코 플러스 
+// @namespace    https://www.fmkorea.com/
+// @version      2401190254
+// @description  add function to fmkorea
+// @author       초연
+// @match        https://www.fmkorea.com/index.php?mid=ib&sort_index=pop&order_type=desc&listStyle=webzine&document_srl=6611021348
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=fmkorea.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("test")
+    // Your code here...
+})();
