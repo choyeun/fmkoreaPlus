ab1d8ae801123675d8d27c22ba643db431055288
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 03:16:25 +0900

    2401190316

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         fmkoreaPlus
-// @name:ko      펨코 플러스 
+// @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
 // @version      2401190307
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
+// @match        https://www.fmkorea.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fmkorea.com
 // @grant        none
 // ==/UserScript==
