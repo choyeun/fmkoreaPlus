e1f106234c4c668606ac8e3c5cf9cb180ba9754c
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 03:02:39 +0900

    Update fmkoreaPlus.js

--- fmkoreaPlus.js
+++ fmkoreaPlus.js
@@ -5,7 +5,7 @@
 // @version      2401190254
 // @description  add function to fmkorea
 // @author       초연
-// @match        https://www.fmkorea.com/index.php?mid=ib&sort_index=pop&order_type=desc&listStyle=webzine&document_srl=6611021348
+// @match        https://www.fmkorea.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fmkorea.com
 // @grant        none
 // ==/UserScript==
