3e1f62b418ad328c04183f6135b286333501f6e7
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 08:17:33 +0900

    0.0.25

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.24
+// @version      0.0.25
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
