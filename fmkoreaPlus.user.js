83844f698a50e863ba4fedccc8e37927dfd6ddf4
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 07:58:51 +0900

    0.0.21

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.20
+// @version      0.0.21
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -18,7 +18,7 @@
 (function () {
     'use strict';
     console.log("펨코 플러스?")
-    GM_addElement(document.getElementsByClassName("member_1686667362 member_plate"), "a", { "class": "member_1686667362 member_plate", "style": "background-color: #000000; color: #ffffff; font-size: 20px; font-weight: bold; text-align: center; padding: 10px 0px;" }, "펨코 플러스")
+    //GM_addElement(document.getElementsByClassName("member_1686667362 member_plate"), "a", { "class": "member_1686667362 member_plate", "style": "background-color: #000000; color: #ffffff; font-size: 20px; font-weight: bold; text-align: center; padding: 10px 0px;" }, "펨코 플러스")
     // Your code here...
     alert(document.getElementsByClassName("author").length)
     alert("aaaaaaaaaaaaaaaaaaaaaaaa")
