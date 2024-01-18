a3f0aaca88ccc05317dfa6cf4d6c1a6dd207c31e
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 08:53:23 +0900

    0.0.37

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.36
+// @version      0.0.37
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -18,17 +18,7 @@
 (function () {
     'use strict';
     console.log("펨코 플러스?")
-    var allElements = document.querySelectorAll('*');
 
-    // 각 요소의 클래스 검사
-    allElements.forEach(function (element) {
-        var classNames = element.classList;
-
-        // 특정 문자열을 포함하는 클래스가 있는지 확인
-        if (classNames && Array.from(classNames).some(className => className.includes('member_'))) {
-            console.log('클래스에 특정 문자열이 포함되어 있습니다:', element);
-        }
-    });
     //////////////////////////////////////////////
     // 확인된 클래스를 담을 배열 초기화
     var foundClasses = [];
