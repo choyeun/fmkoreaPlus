fa55d65c80ba2fea4c539e45154ff36a53d02d47
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 11:50:08 +0900

    0.0.90

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.89
+// @version      0.0.90
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -151,7 +151,7 @@
 
             // element가 null이 아닌 경우에만 innerText를 설정
             if (elementsArray[i]) {
-                if (elementsArray[i].innerText == "★ BEST") { }
+                if (elementsArray2[i].innerText == "★ BEST") { }
                 else {
                     elementsArray[i].innerText = "";
                 }
