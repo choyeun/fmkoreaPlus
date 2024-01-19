e426604a80648aaa819e9d34ef32c75409b81c88
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 09:19:36 +0900

    0.0.46

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.45
+// @version      0.0.46
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -45,7 +45,7 @@
         //console.log('확인된 클래스 배열:', foundClasses[i].value);
         classNames.push(foundClasses[i].value);
         console.log(document.getElementsByClassName(classNames[i])[0].innerText);
-        console.log(document.getElementsByClassName(classNames[i])[0].id);
+        //console.log(document.getElementsByClassName(classNames[i])[0].id);
 
         console.log(document.getElementsByClassName(classNames[i])[0].innerHTML);
         document.getElementsByClassName(classNames[i])[0].innerText = "test"
