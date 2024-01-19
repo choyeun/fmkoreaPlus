3420ed107a0aa29e3a0bd4437cf490e195fd465b
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 10:41:40 +0900

    0.0.60

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.59
+// @version      0.0.60
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -99,31 +99,22 @@
 
         var aaaa = [];
         // 대댓글 감지 안 되서 다시 한번 더
-        var allElements = document.querySelectorAll('*');
-        allElements.forEach(function (element) {
-            var classNames = element.classList;
-
-            // 특정 문자열을 포함하는 클래스가 있는지 확인
-            if (classNames && Array.from(classNames).some(className => className.includes('member_plate'))) {
-                // 배열에 클래스 추가
-                aaaa.push(classNames);
-            }
+        var allElements = document.querySelectorAll('[class^="member"]');
 
 
-        }
 
 
-        );
 
-        for (var i = 0; i < aaaa.length; i++) {
+
+        for (var i = 0; i < allElements.length; i++) {
             //console.log('확인된 클래스 배열:', foundClasses[i].value);
             //console.log(foundfindParent[i]);
-            console.log(document.getElementsByClassName(aaaa[i])[0].innerText);
-            console.log(document.getElementsByClassName(aaaa[i])[0].id);
+            console.log(document.getElementsByClassName(allElements[i])[0].innerText);
+            console.log(document.getElementsByClassName(allElements[i])[0].id);
 
             //console.log(document.getElementsByClassName(foundfindParent[i])[0].innerHTML);
-            document.getElementsByClassName(aaaa[i])[0].innerText = "" // 대댓글 감지
-            document.getElementsByClassName(aaaa[i])[i].innerHTML = "" // 대댓글 감지 2
+            document.getElementsByClassName(allElements[i])[0].innerText = "" // 대댓글 감지
+            document.getElementsByClassName(allElements[i])[i].innerHTML = "" // 대댓글 감지 2
         }
 
 
