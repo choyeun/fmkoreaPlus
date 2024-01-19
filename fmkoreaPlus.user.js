e9d456a1cb43f554e21ca9002b94703e86b999df
choyeun <94655527+choyeun@users.noreply.github.com>
2024-01-19 10:58:45 +0900

    0.0.67

--- fmkoreaPlus.user.js
+++ fmkoreaPlus.user.js
@@ -2,7 +2,7 @@
 // @name         fmkoreaPlus
 // @name:ko      펨코 플러스
 // @namespace    https://www.fmkorea.com/
-// @version      0.0.66
+// @version      0.0.67
 // @description  add function to fmkorea
 // @author       초연
 // @match        https://www.fmkorea.com/
@@ -115,7 +115,7 @@
             //console.log(document.getElementsByClassName(allElements[i])[0]);
 
             //console.log(document.getElementsByClassName(foundfindParent[i])[0].innerHTML);
-            document.getElementsByClassName(allElements[i])[0].innerText = "" // 대댓글 감지
+            //document.getElementsByClassName(allElements[i])[0].innerText = "" // 대댓글 감지
             document.getElementsByClassName(allElements[i])[i].innerText = "" // 대댓글 감지 2
             //document.getElementsByClassName(allElements[0])[i].innerText = "" // 대댓글 감지 3
         }
